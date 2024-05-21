17312dbdff777e35e463b19785a9a451ba52f1b0
Mostafiz1257 <mostafiz1257@gmail.com>
2024-05-21 18:29:49 +0600

    create delete api

--- src/app/modules/orders/order.service.ts
+++ src/app/modules/orders/order.service.ts
@@ -1,11 +1,13 @@
 import { TOrders } from './order.interface';
 import { OrderModel } from './order.model';
 
+//create data into DB
 const createOrderIntoDB = async (order: TOrders) => {
   const result = await OrderModel.create(order);
   return result;
 };
 
+//get all data fromDB
 const getAllOrderFromDB = async (email: string) => {
   if (email) {
     const result = await OrderModel.find({ email });
